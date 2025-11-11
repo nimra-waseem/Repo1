f2168c93a288474916ac90040407aa9008a09aa7
nimra-waseem <nw5163882@gmail.com>
2025-11-11 16:33:35 +0500

    FIRST COMMIT

--- obj/Debug/net8.0/ConsoleApp6.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bb76b5d14968f1bb9a8dc0b63ba3fe7564fa27a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
